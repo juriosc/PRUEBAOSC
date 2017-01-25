ec3056399a3fc227c987527a96205a6a4d56d916
ferosc <fsantacruz@omnisciencia.com>
2017-01-25 18:46:37 -0500

    COMIT

---
+++ WEBOSC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEBOSC.Startup))]
+namespace WEBOSC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
