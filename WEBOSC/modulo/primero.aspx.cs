ae93ea043262d40292225bd50c49d1c25b4c7b98
ferosc <fsantacruz@omnisciencia.com>
2017-01-25 18:56:17 -0500

    comit2

---
+++ WEBOSC/modulo/primero.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEBOSC.modulo
+{
+    public partial class primero : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
